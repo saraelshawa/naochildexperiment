d936f9788c9bba90d615ed535557a327d151e5c8
Sara <selshawa96@gmail.com>
2019-11-20 17:30:01 +0900

    gui

--- tk_nao_test.py
+++ tk_nao_test.py
@@ -1,13 +1,19 @@
 import Tkinter  as tk
 from naoqi import ALProxy    
-
+from sys import exit
+print("here here")
 def fun():
 	tts = ALProxy("ALTextToSpeech", "169.254.124.254", 9559)
 	tts.say("Hello world!")
 	tts.say("I am nao")
 root = tk.Tk()
+root.geometry("500x500")
 frame = tk.Frame(root)
 frame.pack()
+
+def quit():
+    global root
+    root.quit()
 
 button = tk.Button(frame, 
                    text="QUIT", 
@@ -18,5 +24,5 @@ slogan = tk.Button(frame,
                    text="Hello",
                    command=fun)
 slogan.pack(side=tk.LEFT)
-
+print("here")
 root.mainloop()
