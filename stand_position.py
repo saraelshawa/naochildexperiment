c62f30f5fd233cd2fad92a9e1bd94b79e942faa8
Sara <selshawa96@gmail.com>
2019-12-11 15:10:49 +0900

    stand position

--- stand_position.py
+++ stand_position.py
@@ -1,10 +1,9 @@
 #~ This script was generated automatically by drang&drop from Position Library
 from naoqi import ALProxy
-
+from settings import IP_ADDRESS
 def stand_position():
-    print("here")
     try:
-        postureProxy = ALProxy("ALRobotPosture", "169.254.186.86", 9559)
+        postureProxy = ALProxy("ALRobotPosture", IP_ADDRESS, 9559)
     except Exception, e:
         print "Could not create proxy to ALRobotPosture"
         print "Error was: ",
