29faa491db636f50db683a7cf150e0e230980da8
Sara <selshawa96@gmail.com>
2019-12-11 15:10:11 +0900

    cleanup

--- tk_nao_test.py
+++ tk_nao_test.py
@@ -3,6 +3,9 @@ from naoqi import ALProxy
 from sys import exit
 import qi
 import time
+from settings import IP_ADDRESS
+
+
 from GazeFollowInteraction import GazeFollowInteraction
 from hand_wave import hand_wave
 from gangam_style import gangam_style_position, gangam_style_dance
@@ -12,7 +15,6 @@ from stand_position import stand_position
 	# tts = ALProxy("ALTextToSpeech", "169.254.124.254", 9559)
 	# tts.say("Hi, Sho!")
 	
-IP_ADDRESS = "169.254.186.86"
 def quit():
     global root
     root.quit()
@@ -32,27 +34,26 @@ def eye_color():
     leds_service.rasta(1)
 
 
-def moveHead(type, direction):
-    motionProxy = ALProxy("ALMotion", "169.254.124.254", 9559)
+def moveHead(type, direction, angle, time_end):
+    motionProxy = ALProxy("ALMotion", IP_ADDRESS, 9559)
 
     #left or right
     if type == "HeadYaw":
         if direction == "left":
-            angleLists = [[0.5, 0.5]]
+            angleLists = [[angle]*time_end]
         if direction == "right":
-            angleLists = [[-0.5, -0.5]]
-
-        timeLists = [[1.0, 2.0]]
+            angleLists = [[-angle]*time_end]
+        timeLists = [range(1, time_end+1)]
+        print(angleLists, timeLists)
         isAbsolute  = True
         motionProxy.angleInterpolation(["HeadYaw"], angleLists, timeLists, isAbsolute)
     #up or down 
     if type == "HeadPitch":
         if direction == "up":
-            angleLists = [[-0.2, -0.2]]
+            angleLists = [[-angle]*time_end]
         if direction == "down":
-            angleLists = [[0.5, 0.5]]
-            
-        timeLists = [[1.0, 2.0]]
+            angleLists = [[angle]*time_end]
+        timeLists = [range(1, time_end+1)]
         isAbsolute  = True
         motionProxy.angleInterpolation(["HeadPitch"], angleLists, timeLists, isAbsolute)
 
@@ -63,15 +64,18 @@ def make_sound():
     tts = ALProxy("ALTextToSpeech", IP_ADDRESS, 9559)
     tts.say("Ow!")
 
+    leds_service = ALProxy("ALLeds", IP_ADDRESS, 9559)
+    leds_service.rasta(1)
 
 
 gazeFollowInteration = GazeFollowInteraction()
 def gaze_follow():
+    stand_position()
     next_move = gazeFollowInteration.get_next_move()
     if next_move == GazeFollowInteraction.LEFT_HEAD_MOVE:
-        moveHead("HeadYaw", "left")
+        moveHead("HeadYaw", "left", 0.7, 4)
     elif next_move == GazeFollowInteraction.RIGHT_HEAD_MOVE:
-        moveHead("HeadYaw", "right")
+        moveHead("HeadYaw", "right", 0.7, 4)
     return
 
 
@@ -88,9 +92,25 @@ def dance():
 
 def hand_wave_func():
     stand_position()
-    hand_wave()
+    managerProxy = ALProxy("ALBehaviorManager", IP_ADDRESS, 9559)
+    managerProxy.runBehavior("animations/Stand/Gestures/Hey_1")
     # tts = ALProxy("ALAnimationPlayer", IP_ADDRESS, 9559)
     # tts.run("animations/Stand/Gestures/Hey_1!")
+
+
+def changeAngles(name, add_angle):
+    print("in change angles")
+    motionProxy = ALProxy("ALMotion", IP_ADDRESS, 9559)
+
+    useSensors    = True
+    commandAngle = motionProxy.getAngles(name, useSensors)
+    
+    new_pos = [commandAngle[0] + add_angle]
+    motionProxy.angleInterpolationWithSpeed([name], new_pos, 0.1)
+
+    # motionProxy.setAngles(name, new_pos, 0.1)
+    # motionProxy.setStiffnesses("Body", 1.0)
+
 
 root = tk.Tk()
 root.geometry("500x500")
@@ -102,12 +122,11 @@ root.protocol('WM_DELETE_WINDOW', root.quit())
 
 button = tk.Button(frame, text="QUIT", fg="red", command=quit)
 button.pack(side=tk.LEFT)
-# slogan = tk.Button(frame, text="Hello", command=fun)
-# slogan.pack(side=tk.LEFT)
+
 eye_button = tk.Button(frame, text="Eyes", fg="red", command=eye_color)
 eye_button.pack(side=tk.LEFT)
 
-sound_button = tk.Button(frame, text="Sound", fg="red", command=make_sound)
+sound_button = tk.Button(frame, text="Bring Attention", fg="red", command=make_sound)
 sound_button.pack(side=tk.LEFT)
 
 gaze_following_button = tk.Button(frame, text="Gaze Follow", fg="blue", command=gaze_follow)
@@ -122,19 +141,22 @@ wave_hand_button.pack(side=tk.LEFT)
 
 def leftKey(event):
     print "Left key pressed"
-    moveHead("HeadYaw", "left")
+    # moveHead("HeadYaw", "left", 0.2, 2)
+    changeAngles("HeadYaw", 0.3)
+
 
 def rightKey(event):
     print "Right key pressed"
-    moveHead("HeadYaw", "right")
+    changeAngles("HeadYaw", -0.3)
+    # moveHead("HeadYaw", "right", 0.2, 2)
 
 def upKey(event):
     print "Up key pressed"
-    moveHead("HeadPitch", "up")
+    moveHead("HeadPitch", "up", 0.2, 2)
 
 def downKey(event):
     print "Down key pressed"
-    moveHead("HeadPitch", "down")
+    moveHead("HeadPitch", "down", 0.2, 2)
 
 root.bind('<Left>', leftKey)
 root.bind('<Right>', rightKey)
@@ -154,3 +176,4 @@ def get_behaviours():
     allItems = alBehaviorManagerProxy.getRunningBehaviors()
     for i in allItems:
         print i
+
