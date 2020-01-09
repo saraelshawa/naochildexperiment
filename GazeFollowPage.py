032f700d27f354c73c48d0557aea726e9264e6bd
Sara <selshawa96@gmail.com>
2020-01-09 17:00:57 +0900

    gaze following working

--- GazeFollowPage.py
+++ GazeFollowPage.py
@@ -1,5 +1,10 @@
 import Tkinter  as tk
-
+from GazeFollowInteraction import GazeFollowInteraction
+from naoqi import ALProxy
+import time
+from stand_position import stand_position
+from settings import IP_ADDRESS
+from settings import PORT
 
 class GazeFollowPage(tk.Frame):
     def x(self):
@@ -9,5 +14,30 @@ class GazeFollowPage(tk.Frame):
         frame_1 = tk.Frame.__init__(self, master, **kw)
         tk.Frame.configure(self,bg='lightblue')
         tk.Label(self, text="Gaze Follow Page", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        gaze_following_button = tk.Button(self, text="Gaze Follow", fg="blue", command=self.gaze_follow)
+        gaze_following_button.pack(side=tk.LEFT)
+        self.gazeFollowInteration = GazeFollowInteraction()
 
+
+    def gaze_follow(self):
+
+        stand_position()
+        next_move = self.gazeFollowInteration.get_next_move()
+        if next_move == GazeFollowInteraction.LEFT_HEAD_MOVE:
+            self.move_head_diagonal(0.45, 0.45)
+        elif next_move == GazeFollowInteraction.RIGHT_HEAD_MOVE:
+            self.move_head_diagonal(0.45, -0.45)
+        
+        #hold for 5 seconds
+        time.sleep(5)
+        stand_position()
+        return
     
+    def move_head_diagonal(self, angle_up_down, angle_left_right):
+        motionProxy = ALProxy("ALMotion", IP_ADDRESS, PORT)
+
+        angleLists = [angle_up_down, angle_left_right]
+        timeLists = [1,1]
+        isAbsolute  = True
+        motionProxy.angleInterpolation(["HeadPitch", "HeadYaw"], angleLists, timeLists, isAbsolute)
+        
