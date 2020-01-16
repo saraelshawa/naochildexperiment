00184beaacf4bfcbdd8a89fd55c5d1bf6c72f31c
Sara <selshawa96@gmail.com>
2020-01-16 15:56:08 +0900

    start page

--- StartPage.py
+++ StartPage.py
@@ -10,6 +10,7 @@ import json
 class StartPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        
         tk.Label(self, text="Main page", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
         tk.Button(self, text="Social",
                   command=lambda: master.switch_frame(SocialPage)).pack()
